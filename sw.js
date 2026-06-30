af0605b6dcb3fe49a71f1a43cb80dc7e6376b877
sandeepkramesan <sandeepkramesan@gmail.com>
2026-07-01 01:28:26 +0530

    banner location & bug fixes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // TripSplit Service Worker
 // Cache name — bump version here whenever you update the app
-const CACHE = 'tripsplit-v4';
+const CACHE = 'tripsplit-v6';
 
 // Files to pre-cache on install
 const PRECACHE = [
